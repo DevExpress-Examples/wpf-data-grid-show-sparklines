ce1831de9817ba09d5d1ed09ea87bab5f39c1587
DevExpressExampleBot <fakeEmail>
2023-12-06 15:55:47 +0400

    Source auto update [skip ci]

--- VB/SparklineInGrid/MainWindow.xaml.vb
+++ VB/SparklineInGrid/MainWindow.xaml.vb
@@ -1,97 +1,50 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections
 Imports System.Collections.Generic
 Imports System.Collections.ObjectModel
-Imports System.Threading
 Imports System.Windows
-Imports DevExpress.Xpf.Utils
 
 Namespace SparklineInGrid
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private privateSalesData As List(Of SalesDataRow)
-		Public Property SalesData() As List(Of SalesDataRow)
-			Get
-				Return privateSalesData
-			End Get
-			Set(ByVal value As List(Of SalesDataRow))
-				privateSalesData = value
-			End Set
-		End Property
+        Public Property SalesData As List(Of SalesDataRow)
 
-		Public Sub New()
-			InitializeComponent()
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.grid.DataContext = Me
+            SalesData = New List(Of SalesDataRow)()
+            Dim rowsCount As Integer = 20
+            For i As Integer = 0 To rowsCount - 1
+                SalesData.Add(New SalesDataRow() With {.Title = String.Format("Index: {0}", i + 1), .SparklineData = GenerateSparklineData()})
+            Next
+        End Sub
 
-			grid.DataContext = Me
+        Private random As Random = New Random()
 
-			SalesData = New List(Of SalesDataRow)()
+        Private Function GenerateSparklineData() As IList
+            Dim sparklineData As ObservableCollection(Of SalesItem) = New ObservableCollection(Of SalesItem)()
+            Dim dateTime As Date = New DateTime(2013, 07, 17)
+            For i As Integer = 0 To 30 - 1
+                sparklineData.Add(New SalesItem() With {.ValueColumn = random.Next(-20, 20), .ArgumentColumn = dateTime.AddDays(i)})
+            Next
 
-			Dim rowsCount As Integer = 20
+            Return sparklineData
+        End Function
+    End Class
 
-			For i As Integer = 0 To rowsCount - 1
-				SalesData.Add(New SalesDataRow() With {.Title = String.Format("Index: {0}", i+1), .SparklineData = GenerateSparklineData()})
-			Next i
+    Public Class SalesDataRow
 
-		End Sub
+        Public Property Title As Object
 
-		Private random As New Random()
+        Public Property SparklineData As IList
+    End Class
 
-		Private Function GenerateSparklineData() As IList
-			Dim sparklineData As New ObservableCollection(Of SalesItem)()
+    Public Class SalesItem
 
-			Dim dateTime As New DateTime(2013, 07, 17)
+        Public Property ArgumentColumn As Object
 
-			For i As Integer = 0 To 29
-				sparklineData.Add(New SalesItem() With {.ValueColumn = random.Next(-20, 20), .ArgumentColumn = dateTime.AddDays(i)})
-			Next i
-
-			Return sparklineData
-		End Function
-
-	End Class
-
-	Public Class SalesDataRow
-		Private privateTitle As Object
-		Public Property Title() As Object
-			Get
-				Return privateTitle
-			End Get
-			Set(ByVal value As Object)
-				privateTitle = value
-			End Set
-		End Property
-		Private privateSparklineData As IList
-		Public Property SparklineData() As IList
-			Get
-				Return privateSparklineData
-			End Get
-			Set(ByVal value As IList)
-				privateSparklineData = value
-			End Set
-		End Property
-	End Class
-
-	Public Class SalesItem
-		Private privateArgumentColumn As Object
-		Public Property ArgumentColumn() As Object
-			Get
-				Return privateArgumentColumn
-			End Get
-			Set(ByVal value As Object)
-				privateArgumentColumn = value
-			End Set
-		End Property
-		Private privateValueColumn As Integer
-		Public Property ValueColumn() As Integer
-			Get
-				Return privateValueColumn
-			End Get
-			Set(ByVal value As Integer)
-				privateValueColumn = value
-			End Set
-		End Property
-	End Class
+        Public Property ValueColumn As Integer
+    End Class
 End Namespace
